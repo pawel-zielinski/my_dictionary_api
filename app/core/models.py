bfd99d3fd8bddc4f1e78d71e06f82b0e82da8a06
Pawel Zielinski <pawelzet00.pz@gmail.com>
2023-10-17 16:56:34 +0200

    Add string representation to all models

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User as DjangoUser
 class User(DjangoUser):
     profile = models.ForeignKey('Profile', on_delete=models.RESTRICT, related_name='profile')
 
+    def __str__(self):
+        return f'({self.profile}) {self.username} -- {self.email}'
+
 
 class Event(models.Model):
     name = models.CharField(max_length=100)
@@ -15,10 +18,19 @@ class Event(models.Model):
     attachment = models.FileField()
     notes = models.CharField(max_length=250)
 
+    def __str__(self):
+        return f'[{self.tags}] ({self.date}) {self.name}'  # TODO: Check these tags and date printout.
+
 
 class Profile(models.Model):
     name = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name
--- app/core/viewsets.py
+++ app/core/viewsets.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
 from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin, RetrieveModelMixin, CreateModelMixin
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from core.models import Event, User
 from core.serializers import HomeSerializer, EventSerializer, UserSerializer
 
 
-class HomeViewSet(CreateModelMixin, RetrieveModelMixin):
-    class Meta:
-        queryset = Event.objects.all()
-        serializer_class = HomeSerializer
+class HomeViewSet(GenericViewSet, CreateModelMixin, RetrieveModelMixin):
+    queryset = Event.objects.all()
+    serializer_class = HomeSerializer
 
 
-class EventViewSet(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
-    class Meta:
-        queryset = Event.objects.all()
-        serializer_class = EventSerializer
+class EventViewSet(GenericViewSet, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
 
-class UserViewSet(RetrieveModelMixin, UpdateModelMixin):
-    class Meta:
-        queryset = User.objects.all()
-        serializer_class = UserSerializer
+class UserViewSet(GenericViewSet, RetrieveModelMixin, UpdateModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
