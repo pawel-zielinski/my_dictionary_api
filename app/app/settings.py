50db657261766d91f7607cc7c253a8720fe35231
Piotr-Zielinski-PZ <piotrzet00@outlook.com>
2023-10-21 23:59:32 +0200

    Create basic login/register endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -52,7 +52,10 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 ROOT_URLCONF = 'app.urls'
