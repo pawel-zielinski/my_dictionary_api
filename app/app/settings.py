09e2bf5d5cbc34f5159a9ffee96752354246ea1a
Pawel Zielinski <pawelzet00.pz@gmail.com>
2023-10-22 17:34:23 +0200

    Add bcrypt to improve pasword security

--- app/app/settings.py
+++ app/app/settings.py
@@ -58,6 +58,16 @@ REST_FRAMEWORK = {
     ],
 }
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
