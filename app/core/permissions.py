d88d8a1f7a0464e26839b95c05f8fb3bff0a81a5
Pawel Zielinski <pawelzet00.pz@gmail.com>
2023-10-22 16:48:23 +0200

    Add user permission and mess with enhancements

--- app/core/permissions.py
+++ app/core/permissions.py
@@ -3,9 +3,20 @@ from rest_framework import permissions
 
 class EventPermission(permissions.BasePermission):
 
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            return request.user.profile.name == 'organizer'
+        return True
+
     def has_object_permission(self, request, view, obj):
-        if view.action == 'retrieve':
-            return True
         if view.action in ['update', 'destroy']:
             return obj.organizer == request.user
-        return False
+        return True
+
+
+class UserPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if view.action in ['update', 'destroy']:
+            return obj == request.user
+        return True
--- app/core/viewsets.py
+++ app/core/viewsets.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin, RetrieveModelMixin, CreateModelMixin
+from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin, RetrieveModelMixin, CreateModelMixin, \
+    ListModelMixin
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.viewsets import GenericViewSet
 
 from core.models import Event, User
-from core.permissions import EventPermission
+from core.permissions import EventPermission, UserPermission
 from core.serializers import HomeSerializer, EventSerializer, UserSerializer
 
 
-class HomeViewSet(GenericViewSet, CreateModelMixin, RetrieveModelMixin):
+class HomeViewSet(GenericViewSet, CreateModelMixin, ListModelMixin):
     queryset = Event.objects.all()
     serializer_class = HomeSerializer
+    permission_classes = (IsAuthenticated, EventPermission)
 
 
 class EventViewSet(GenericViewSet, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
@@ -22,3 +24,4 @@ class EventViewSet(GenericViewSet, RetrieveModelMixin, UpdateModelMixin, Destroy
 class UserViewSet(GenericViewSet, RetrieveModelMixin, UpdateModelMixin):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, UserPermission,)
